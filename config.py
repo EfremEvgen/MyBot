798bd7ee3430afd7b045b0f8005d9649f8529e8a
EfremEvgen <EfremenkoEvgenii1@gmail.com>
2023-05-31 12:59:21 +0500

    Первый коммит

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '6249133798:AAGExpeJVVd8kgggcxaOZMYQ0Faabo7bZUw'
+
+keys = {
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'йена': 'JPY',
+    'тенге': 'KZT',
+    'фунт': 'GBP',
+    'лира': 'TRY'
+}
